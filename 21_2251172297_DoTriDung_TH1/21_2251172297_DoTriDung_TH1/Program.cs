1cf24172819868166b0b05ca9308c5e692d165ab
dotridung2004 <dtd16102004@gmail.com>
2025-05-09 13:11:42 +0700

    21_2251172297_DoTriDung

---
+++ 21_2251172297_DoTriDung_TH1/21_2251172297_DoTriDung_TH1/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+class Program
+{
+    static int[] A;
+    static int[] maxResults;
+    static int threadCount = 4;
+
+    static void Main()
+    {
+        int N = 120;
+        A = new int[N];
+        maxResults = new int[threadCount];
+        SinhMangNgauNhien(N);
+        InMang();
+
+        Console.WriteLine("\n--- Bat dau xu ly da luong ---\n");
+        XuLyDaLuong(N);
+
+        int ketQuaCuoi = TimMaxCuoiCung();
+        Console.WriteLine($"\n==> Gia tri lon nhat trong mang: {ketQuaCuoi}");
+    }
+
+
+    static void SinhMangNgauNhien(int N)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < N; i++)
+        {
+            A[i] = rand.Next(1, 1000); 
+        }
+    }
+
+    static void InMang()
+    {
+        Console.WriteLine("Mang A:");
+        Console.WriteLine(string.Join(", ", A));
+    }
+
+    static void XuLyDaLuong(int N)
+    {
+        int chunkSize = N / threadCount;
+        List<Thread> threads = new List<Thread>();
+
+        for (int i = 0; i < threadCount; i++)
+        {
+            int threadIndex = i;
+            int start = threadIndex * chunkSize;
+            int end = (threadIndex == threadCount - 1) ? N : start + chunkSize;
+
+            Thread t = new Thread(() => XuLyDoan(threadIndex, start, end));
+            threads.Add(t);
+            t.Start();
+        }
+
+        
+        foreach (var t in threads)
+            t.Join();
+    }
+
+    static void XuLyDoan(int threadIndex, int start, int end)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+
+        int localMax = A[start];
+        for (int j = start + 1; j < end; j++)
+        {
+            if (A[j] > localMax)
+                localMax = A[j];
+        }
+
+        sw.Stop();
+        maxResults[threadIndex] = localMax;
+
+        Console.WriteLine($"T{threadIndex + 1}: {localMax} - {DateTime.Now:HH:mm}");
+        
+    }
+
+    static int TimMaxCuoiCung()
+    {
+        int max = maxResults[0];
+        for (int i = 1; i < maxResults.Length; i++)
+        {
+            if (maxResults[i] > max)
+                max = maxResults[i];
+        }
+        return max;
+    }
+}
---
+++ 21_2251172297_DoTriDung_TH1/21_2251172297_DoTriDung_TH1/Program2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_2251172297_DoTriDung_TH1
+{
+    internal class Program2
+    {
+    }
+}
